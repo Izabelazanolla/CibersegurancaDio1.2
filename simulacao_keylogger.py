08c338917c745372b6379ebf486c378e204797d0
Izabelazanolla <165805295+Izabelazanolla@users.noreply.github.com>
2025-11-30 14:28:09 -0400

    Create simulacao_keylogger.py

---
+++ simulacao_keylogger.py
@@ -0,0 +1,1 @@
+texto_simulado = "Exemplo de teclas digitadas para fins educativos." with open("keylog_simulado.txt", "w") as f: for letra in texto_simulado: f.write(letra + "\n") print("Keylog simulado salvo em keylog_simulado.txt")
